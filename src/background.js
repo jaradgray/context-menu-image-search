887d44cd562464a126a3c472e17940187fc88c11
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-11 01:21:41 -0600

    it's functional now

--- src/background.js
+++ src/background.js
@@ -16,14 +16,25 @@ browser.contextMenus.create({
 // Handle clicks on context menu items
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    // detect one of our image search items clicked
+    // Set the base of the search URL based on which context menu item was clicked
+    let baseUrl = "";
     switch(info.menuItemId) {
         case "image-search-tineye":
-            console.log("TODO: search via tineye");
+            console.log("Search via tineye for " + info.srcUrl);
+            baseUrl = "https://tineye.com/search?url=";
             break;
 
         case "image-search-google":
-            console.log("TODO: search via google");
+            console.log("Search via google for " + info.srcUrl);
+            baseUrl = "https://www.google.com/searchbyimage?site=search&sa=X&image_url=";
             break;
+    }
+
+    // If one of our image search context menu items was clicked...
+    if (baseUrl !== "") {
+        // ...open a new tab to display the search results
+        browser.tabs.create({
+            url: baseUrl + info.srcUrl
+        });
     }
 });
